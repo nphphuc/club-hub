6e291d82206a0bcf6a8aa6cfd8bc1971af92d1d2
nphphuc <pavilionforc@gmail.com>
2026-07-05 23:50:18 +0700

    update stuffs

--- backend/src/ClubHub.API/Program.cs
+++ backend/src/ClubHub.API/Program.cs
@@ -15,6 +15,8 @@ Env.Load(Path.Combine(Directory.GetCurrentDirectory(), ".env"));
 
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Configuration.AddEnvironmentVariables();
+
 // ── Database ──────────────────────────────────────────────────────────────────
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection"),
--- backend/src/ClubHub.API/obj/Debug/net8.0/ClubHub.API.AssemblyInfo.cs
+++ backend/src/ClubHub.API/obj/Debug/net8.0/ClubHub.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubHub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f45df38282e600484af42b5a1470f40092f47e27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd310bbc699b963c96ea6e7f8ec975b98762f73")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubHub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubHub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
